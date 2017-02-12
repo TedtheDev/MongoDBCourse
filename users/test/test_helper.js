07832c69886c696ea987a18c9eda8c0ea8ef11d9
TedtheDev <tylerjedmunds@gmail.com>
2017-02-12 11:57:13 -0600

    fixed typo, connction to connection...

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/users_test');
-mongoose.connction
+mongoose.connection
   .once('open', () => console.log('Good to go!'))
   .on('error', (error) => {
     console.warn('Warning', error);

c73460625fbd41673b34fc91e0bc0bfe8f3f7d00
TedtheDev <tylerjedmunds@gmail.com>
2017-02-17 00:02:34 -0600

    added model class update, findOneAndUpdate, findByIdAndUpdate to assert

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -28,6 +28,30 @@ describe('Updating records', () => {
   });
 
   it('a model instance can update', (done) => {
-    assertName(joe.update({ name: 'Alex'}), done);
+    assertName(
+      joe.update({ name: 'Alex'}),
+      done
+    );
+  });
+
+  it('a model class can update', (done) => {
+    assertName(
+      User.update({ name: 'Joe'}, {name: 'Alex'}),
+      done
+    );
+  });
+
+  it('a model class can update one record', (done) => {
+    assertName(
+      User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }),
+      done
+    );
+  });
+
+  it('a model class can find a record with an Id and update', (done) => {
+    assertName(
+      User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
+      done
+    );
   });
 });

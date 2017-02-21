b4c4c66d74599f76c09554739497e25aed4d5d39
TedtheDev <tylerjedmunds@gmail.com>
2017-02-20 21:11:35 -0600

    added validation to name property on User schema and added it to validation_tests

--- users/src/user.js
+++ users/src/user.js
@@ -4,7 +4,10 @@ mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: [true, 'Name is required.']
+  },
   postCount: Number
 });
 
---
+++ users/test/validation_test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Validating records', () => {
+  it('requires a user name', () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name is required.');
+  });
+
+  it('requires a user name', () => {
+
+  });
+
+});

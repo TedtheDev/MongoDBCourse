e0cb5770cba788b61d404731ddde3500c703e8c8
TedtheDev <tylerjedmunds@gmail.com>
2017-03-01 21:17:21 -0600

    added 'saves a full relations graph' to association_test, fixed typo in comment from conent to content

--- users/src/comment.js
+++ users/src/comment.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema ({
-  conent: String,
+  content: String,
   user: {
     type: Schema.Types.ObjectId,
     ref: 'user'

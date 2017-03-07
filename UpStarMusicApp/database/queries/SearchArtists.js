825b4a04a009d4e86e431895b9fcf1c701dd61c2
TedtheDev <tylerjedmunds@gmail.com>
2017-03-06 22:32:33 -0600

    added buildQuery function to build query based on criteria object

--- UpStarMusicApp/database/queries/SearchArtists.js
+++ UpStarMusicApp/database/queries/SearchArtists.js
@@ -9,11 +9,10 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
-  const query = Artist.find({})
+  const query = Artist.find(buildQuery(criteria))
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit);
-
   return Promise.all([query, Artist.count()])
     .then((results) => {
       return {
@@ -23,4 +22,25 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
         limit: limit
       };
     });
+};
+
+
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if(criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    }
+  }
+
+  if(criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    }
+  }
+
+  return query;
 };

ac389a46c3d91b972bac128c2fbc65f853d77769
TedtheDev <tylerjedmunds@gmail.com>
2017-03-07 23:04:28 -0600

    added models folder and created Driver Schema and Model

---
+++ muberApp/models/driver.js
@@ -0,0 +1,16 @@
+const mongoose, { Schema } = require('mongoose');
+
+const DriverSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  driving: {
+    type: boolean
+    default: false
+  }
+});
+
+const Driver = mongoose.model('driver', DriverSchema);
+
+module.exports = Driver;

53d57cf29e6a4e230c5f34b2d0fec37527a0c044
TedtheDev <tylerjedmunds@gmail.com>
2017-03-07 23:10:59 -0600

    added POST api/drivers to routes and controllers

--- muberApp/routes/routes.js
+++ muberApp/routes/routes.js
@@ -1,7 +1,12 @@
 const DriversController = require('../controllers/drivers_controller');
 
 module.exports = (app) => {
-  // Watch for incoming requests of method get
+  // Watch for incoming requests of method GET
   // to the route https://localhost:3050/api
   app.get('/api', DriversController.greeting);
+
+  // Watch for incoming requests of method POST
+  // to the route https://localhost:3050/api/drivers
+  app.post('/api/drivers', DriversController.create);
+
 }

aa8361695aaa553ba773c33c84a79223bc38845b
TedtheDev <tylerjedmunds@gmail.com>
2017-03-07 22:08:52 -0600

    added /api to test GET method

--- muberApp/app.js
+++ muberApp/app.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// Watch for incoming requests of method get
+// to the route https://localhost:3050/api
+app.get('/api', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
 module.exports = app;
